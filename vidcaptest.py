04457b63da46c831cfeda94eecea5b3e9e54feeb
SWiT <switlikm@gmail.com>
2012-04-09 23:18:17 -0400

    better

--- vidcaptest.py
+++ vidcaptest.py
@@ -12,10 +12,15 @@ success, prevImg = cap.read()
 drawcolors = ((255,0,0), (0,255,0), (0,0,255))
 i = 0
 outputgray = False
-drawPts = False
+printPts = False
+
+detectionModes = (["Off", "goodFeaturesToTrack"])
+dm = 0
 
 print "'Esc' to exit."
 print "'g' to toggle grayscaled output."
+print "'d' to change point detection mode."
+print "'p' to output points"
 
 while True:
     success, nextImg = cap.read()
@@ -26,43 +31,62 @@ while True:
     key = cv2.waitKey(1)        
     if key == 27:  #esc key
         break
+    
+    elif key == 100: #d key
+        dm += 1
+        if dm >= len(detectionModes):
+            dm = 0
+        print "Point Detection "+detectionModes[dm]
+        
     elif key == 103: #g key
-        print "Grayscale "+str(outputgray)
         outputgray = not outputgray
+        print "Grayscale "+str(outputgray)
+        
     elif key == 112: #p key
-        print "Draw Points "+str(drawPts)
-        drawPts = not drawPts
+        printPts = not printPts
+        print "Output Points "+str(printPts)
+        
     elif key > 0:
         print "unknown key "+str(key)
+
 
     #Set output to Grayscale or Color
     if outputgray:
         outputImg = grayImg
     else:
         outputImg = nextImg
+        
 
-    #Draw detected points on the output image
-    if drawPts:
+    #Point Detetcion Mode
+    if detectionModes[dm]=="goodFeaturesToTrack":
         nextPts = cv2.goodFeaturesToTrack(grayImg, 300, 0.01, 10)
-        drawcolor = drawcolors[i]
-        i=i+1
-        if i>=3:
-            i=0
-        
-        for obj in nextPts:
-            for x,y in obj:    
-                x = int(x)
-                y = int(y)
-                cv2.circle(outputImg, (x,y), 3, drawcolor, -1, 8, 0)
+    elif detectionModes[dm]=="Off":
+        nextPts = array([])
 
-        
-    
+
+    #Output Points
+    if printPts:
+        print nextPts
+
+
+    #Draw points on output
+    drawcolor = drawcolors[i]
+    i+=1
+    if i>=3:
+        i=0
+    for obj in nextPts:
+        for x,y in obj:    
+            x = int(x)
+            y = int(y)
+            cv2.circle(outputImg, (x,y), 3, drawcolor, -1, 8, 0)
+
+    #Draw output        
     cv2.imshow("ArenaScanner", outputImg)
 
     prevPts = nextPts
     prevImg = nextImg
 
-    
+print "Exiting."    
 
 cap.release()
 cv2.destroyAllWindows() 
