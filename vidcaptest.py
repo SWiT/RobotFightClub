3ca570c5c77e125cdfde408dd25b9a6f9d13dcf1
SWiT <switlikm@gmail.com>
2012-04-12 20:54:43 -0400

    Pts arrays forced to float32 to fix clickDefinedPoints errors

--- vidcaptest.py
+++ vidcaptest.py
@@ -4,18 +4,22 @@ from numpy import *
 cap = cv2.VideoCapture(1)
 cv2.namedWindow("ArenaScanner")
 key = -1
-prevPts = array([])
-nextPts = array([])
+prevPts = array([], dtype=float32)
+nextPts = prevPts.copy()
+
 success, prevImg = cap.read()
 
 outputgray = False
 printPts = False
 blurInput = False
 
-detectionModes = (["Off","goodFeaturesToTrack","calcOpticalFlowPyrLK","clickDefinedPoints"])
+detectionModes = (["Off"
+                   ,"clickDefinedPoints"
+                   ,"goodFeaturesToTrack"
+                   ,"calcOpticalFlowPyrLK"])
 dm = 0
 
-drawColors = ((255,0,0), (0,255,0), (0,0,255))
+drawColors = ((255,0,0), (0,240,0), (0,0,255), (0,210,210))
 dc = 0
 
 
@@ -31,17 +35,20 @@ def onMouse (event, x, y, flags, param):
     global prevPts
     global nextPts
     if event == cv2.EVENT_LBUTTONUP:
-##        print "LBUTTONUP", event, x, y, flags, param
+        #print "LBUTTONUP", event, x, y, flags, param
         s = prevPts.shape
         ni = s[0]
         ns = ni + 1
         prevPts.resize((ns,1,2))
         prevPts.itemset((ni,0,0), x)
         prevPts.itemset((ni,0,1), y)
+
         nextPts = prevPts.copy()
         print nextPts
+
     
 cv2.setMouseCallback("ArenaScanner", onMouse, nextPts)
+
 
 while True:
     success, nextImg = cap.read()
@@ -67,8 +74,8 @@ while True:
         dm += 1
         if dm >= len(detectionModes):
             dm = 0
-        prevPts = array([])
-        nextPts = array([])
+        prevPts = array([], dtype=float32)
+        nextPts = array([], dtype=float32)
         print "Point Detection "+detectionModes[dm]
         
     elif key == 103: #g key
@@ -97,16 +104,14 @@ while True:
     if detectionModes[dm]=="Off":
         nextPts = array([])
     elif detectionModes[dm]=="goodFeaturesToTrack":
-        nextPts = cv2.goodFeaturesToTrack(grayImg, 3, 0.01, 10)  
+        nextPts = cv2.goodFeaturesToTrack(grayImg, 150, 0.01, 10)  
     elif detectionModes[dm]=="calcOpticalFlowPyrLK":
         if len(nextPts)==0:
-            nextPts = cv2.goodFeaturesToTrack(grayImg, 3, 0.01, 10)
+            nextPts = cv2.goodFeaturesToTrack(grayImg, 150, 0.01, 10)
             prevPts = nextPts.copy()
         nextPts,status,err = cv2.calcOpticalFlowPyrLK(prevImg, nextImg, prevPts, nextPts)
     elif detectionModes[dm]=="clickDefinedPoints":
         if len(prevPts)>0 and len(nextPts)>0:
-            print "prev",prevPts
-            print "next",nextPts
             nextPts,status,err = cv2.calcOpticalFlowPyrLK(prevImg, nextImg, prevPts, nextPts)
             
     #Output Points
