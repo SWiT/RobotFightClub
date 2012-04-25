139875ca36c18ab879e80ba758d33ed5f5001951
SWiT <switlikm@gmail.com>
2012-04-25 15:28:59 -0400

    start by making a find points function

--- rfcarena.py
+++ rfcarena.py
@@ -2,6 +2,10 @@ import cv2
 import sys, math
 from numpy import *
 
+###############
+## FUNCTIONS
+###############
+
 def onMouse (event, x, y, flags, param):
     #print event, x, y, flags, param
     global objectsPts
@@ -30,12 +34,12 @@ def resetAllData ():
                 ,[(0,0),(0,0),(0,0)]
                 ,[(0,0),(0,0),(0,0)]
                 ,[(0,0),(0,0),(0,0)]
-                ], dtype=int32)
+                ], dtype=float32)
     return data
 
 
 def resetObjData ():
-    data = array([(0,0),(0,0),(0,0)], dtype=int32)
+    data = array([(0,0),(0,0),(0,0)], dtype=float32)
     return data
 
 
@@ -88,69 +92,72 @@ def drawObject(objPts, colorCode):
             if sz>2:
                 sz = 2
             px,py = x,y
+
             
-
-
-def adjObjectPts(objPts, Img):
-    return objPts
-    adjPts = objPts.copy()
-    i=0
-    imgshape = Img.shape
-    for x,y in objPts:
-        if x<=0 and y<=0:
-            adjPts[i] = (0,0)
-        else:
-            if Img[y][x] == 255:
-                #find center of blob.
-                px,py = x,y
-                while Img[py][px] == 255:
-                    py += 1 #down
-                    if py >= imgshape[0]:
-                        break
-                max_y = py-1
-
-                px,py = x,y
-                while  Img[py][px] == 255:
-                    py -= 1 #up
-                    if py < 0:
-                        break
-                min_y = py+1
-                
-                px,py = x,y
-                while  Img[py][px] == 255:
-                    px += 1 #right
-                    if px >= imgshape[1]:
-                        break
-                max_x = px-1
-
-                px,py = x,y
-                while  Img[py][px] == 255:
-                    px -= 1 #left
-                    if px < 0:
-                        break
-                min_x = px+1
-
-                px = min_x + (max_x-min_x)/2
-                py = min_y + (max_y-min_y)/2
-                adjPts[i] = (px,py)
-            else:
-                print i,x,y,"point fell off..."
-                if i!=0:
-                    print Img[objPts[0][0]][objPts[0][1]] == 255
-                if i!=1:
-                    print Img[objPts[1][0]][objPts[1][1]] == 255
-                if i!=2:
-                    print Img[objPts[2][0]][objPts[2][1]] == 255
-                adjPts[i] = (0,0)
-        i += 1
-    return adjPts
-
-
 def dist(p0, p1):
   return math.sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
 
+
+def findCenterOfBlob(prevPt, color):
+    nextPt = prevPt
+    passes = 0
+    while passes < 3:
+        px,py = prevPt
+        while Img[py][px] == color:
+            py += 1 #down
+            if py >= imgshape[0]:
+                break
+        max_y = py-1
+
+        px,py = prevPt
+        while  Img[py][px] == color:
+            py -= 1 #up
+            if py < 0:
+                break
+        min_y = py+1
+        
+        px,py = prevPt
+        while  Img[py][px] == color:
+            px += 1 #right
+            if px >= imgshape[1]:
+                break
+        max_x = px-1
+
+        px,py = prevPt
+        while  Img[py][px] == color:
+            px -= 1 #left
+            if px < 0:
+                break
+        min_x = px+1
+
+        nextPt[0] = min_x + (max_x-min_x)/2
+        nextPt[1] = min_y + (max_y-min_y)/2
+
+        if nextPt==prevPt:
+            break
+        passes+=1
+        
+    return nextPt
+
+
+def findBots(Img):
+    y=0
+    for row in Img:
+        x=0
+        for pixel in row:
+            if pixel==color:
+                print "found",color,"at",x,",",y
+                #cenPnt = findCenterOfBlob((x,y),255)
+            x+=1
+        y+=1
+
+
+
+
             
-##Setup Instructions
+###############
+## SETUP
+###############            
 if len(sys.argv) > 1:
   cap = cv2.VideoCapture(sys.argv[1])
 else:
@@ -179,9 +186,59 @@ success, prevImg = cap.read()
 displayMenu()
 displayStatuses()
 
-##Run Loop
+
+###############
+## LOOP
+###############
 while True:
+    #get next frame from capture device
+    success, nextImg = cap.read()
+    if not success:
+        break;
+
+
+    #Apply image transformations
+    if dataview == 0:
+        outputImg = nextImg.copy()
+
+    nextImg = cv2.cvtColor(nextImg, cv2.COLOR_BGR2GRAY)
+    if dataview == 1:
+        outputImg = nextImg.copy()
+
+    nextImg = cv2.dilate(nextImg, None, iterations=1)
+    if dataview == 2:
+        outputImg = nextImg.copy()
+
+    retval,nextImg = cv2.threshold(nextImg, 170, 255, cv2.THRESH_BINARY)
+    if dataview == 3:
+        outputImg = nextImg.copy()
         
+
+   
+    #Point Tracking and Detection
+    if tracking:
+        prevPts = float32(objectsPts.reshape((12,2)))
+        nextPts = zeros((12,2),dtype=float32)
+        nextPts,status,err = cv2.calcOpticalFlowPyrLK(prevImg, nextImg, prevPts, nextPts)
+        objectsNextPts = int32(nextPts.reshape((4,3,2)))
+        objectsPts = objectsNextPts.copy()
+            
+            
+    #Draw points on output
+    if drawObjects:
+        objIndex = 0
+        for objPts in objectsPts:
+            drawObject(objPts, objectsColorCode[objIndex])
+            objIndex+=1
+        
+    if drawing:
+        cv2.circle(outputImg, pointer, 2, objectsColorCode[objectIndex], -1, 8, 0)
+
+        
+    #Draw output        
+    cv2.imshow("ArenaScanner", outputImg)
+
+    
     #process key presses        
     key = cv2.waitKey(1)        
     if key == 27 or key == 32:  #esc or spacebar
@@ -198,6 +255,9 @@ while True:
         drawing = not drawing
         displayStatuses()
 
+    elif key == 102: #f key
+        findBots(nextImg)
+        
     elif key == 104: #h key
         displayMenu()
 
@@ -232,60 +292,8 @@ while True:
     elif key > 0:
         displayPrint("unknown key",key)
 
-
-    #get next frame from capture device
-    success, nextImg = cap.read()
-    if not success:
-        break;
-
-
-    #Apply image transformations
-    if dataview == 0:
-        outputImg = nextImg.copy()
-
-    nextImg = cv2.cvtColor(nextImg, cv2.COLOR_BGR2GRAY)
-    if dataview == 1:
-        outputImg = nextImg.copy()
-
-    nextImg = cv2.dilate(nextImg, None, iterations=2)
-    if dataview == 2:
-        outputImg = nextImg.copy()
-
-    retval,nextImg = cv2.threshold(nextImg, 165, 255, cv2.THRESH_BINARY)
-    if dataview == 3:
-        outputImg = nextImg.copy()
-
-   
-    #Point Tracking and Detection
-    objIndex = 0
-    for objPts in objectsPts:
-        objectsPts[objIndex] = adjObjectPts(objectsPts[objIndex], nextImg)
-        objIndex += 1
-        
-    if tracking:
-        prevPts = float32(objectsPts.reshape((12,2)))
-        nextPts = zeros((12,2),dtype=float32)
-        nextPts,status,err = cv2.calcOpticalFlowPyrLK(prevImg, nextImg, prevPts, nextPts)
-        objectsNextPts = int32(nextPts.reshape((4,3,2)))
-        objectsPts = objectsNextPts.copy()
-            
-            
-    #Draw points on output
-    if drawObjects:
-        objIndex = 0
-        for objPts in objectsPts:
-            drawObject(objPts, objectsColorCode[objIndex])
-            objIndex+=1
-        
-    if drawing:
-        cv2.circle(outputImg, pointer, 2, objectsColorCode[objectIndex], -1, 8, 0)
-        
-    #Draw output        
-    cv2.imshow("ArenaScanner", outputImg)
-
     prevImg = nextImg.copy()
-
 print "Exiting."    
 
 cap.release()
-cv2.destroyAllWindows() 
+cv2.destroyAllWindows()
